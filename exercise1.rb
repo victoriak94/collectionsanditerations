533f750e1834381afa962a1c388603533aa83bd7
victoriak94 <vkamila6@gmail.com>
2018-11-14 16:27:32 -0500

    Created arrays and hashes for exercise 0. Ran code in terminal to check for syntax errors. Started exercise 1. Printed flip coin array and the first string in the fav colours array.

---
+++ exercise1.rb
@@ -0,0 +1,14 @@
+
+fav_colours = ['green', 'black', 'yellow', 'red', 'gold']
+age_siblings = [26, 38, 22, 60]
+flip_coin = ['heads', 'heads', 'tails', 'heads', 'tails']
+fav_artists = ['noname', 'frank ocean', 'kamasi washington']
+fav_colours_symbols = [:green, :black, :yellow, :red, :gold]
+
+puts flip_coin
+puts fav_colours[0]
+
+finnish_dictionary = {aave: 'ghost', soturi: 'warrior', sisu: 'stoic determination'}
+fav_movies = {before_sunset: 2004, the_handmaiden: 2016, rear_window: 1954}
+world_cities = {london: 8136000, tokyo: 9273000, hong_kong: 7392000}
+age_cousins = {jack: 22, sandra: 28, helen: 26, patrick: 22}
