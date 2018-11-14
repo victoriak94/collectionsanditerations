90a493aa26d54c9321a27f06c717ae77912844f1
victoriak94 <vkamila6@gmail.com>
2018-11-14 16:58:46 -0500

    Printed output of age siblings sorted. Added baby to age of siblings aged 0 by pushing 0 to the array. Printed the definition of aave.

--- exercise1.rb
+++ exercise1.rb
@@ -7,8 +7,13 @@ fav_colours_symbols = [:green, :black, :yellow, :red, :gold]
 
 puts flip_coin
 puts fav_colours[0]
+puts age_siblings.sort
+age_siblings << 0
 
-finnish_dictionary = {aave: 'ghost', soturi: 'warrior', sisu: 'stoic determination'}
-fav_movies = {before_sunset: 2004, the_handmaiden: 2016, rear_window: 1954}
-world_cities = {london: 8136000, tokyo: 9273000, hong_kong: 7392000}
-age_cousins = {jack: 22, sandra: 28, helen: 26, patrick: 22}
+
+finnish_dictionary = { :aave => 'ghost', :soturi => 'warrior', :sisu => 'stoic determination' }
+fav_movies = { :before_sunset => 2004, :the_handmaiden => 2016, :rear_window => 1954 }
+world_cities = { :london => 8136000, :tokyo => 9273000, :hong_kong => 7392000 }
+age_cousins = { :jack => 22, :sandra => 28, :helen => 26, :patrick => 22 }
+
+puts finnish_dictionary[:aave]
