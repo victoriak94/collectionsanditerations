27366930fe70d3ceff70deb87aed6e80c6f3799b
victoriak94 <vkamila6@gmail.com>
2018-11-15 14:29:36 -0500

    Working through exercise 6 and above still.

--- exercise1.rb
+++ exercise1.rb
@@ -28,22 +28,33 @@ new_age_siblings << age_siblings.sort
 new_age_siblings.reverse
 puts new_age_siblings
 
-puts age_siblings[0]
-puts age_siblings[2]
-puts age_siblings[-1]
+puts age_siblings.select {|age| age < 30}
 
 puts flip_coin.count('heads')
 
 fav_artists.delete('noname')
 puts fav_artists
 
+puts fav_colours[-2..-1]
+fav_colours << "blue"
+fav_colours << "orange"
+puts fav_colours
+
+def add_ages(array)
+  array.each do |age|
+    array[age] += 1
+  end
+  return array
+end
+
+puts add_ages(age_siblings)
 
 puts "\n -------- \n\n"
 
 finnish_dictionary = { :aave => 'ghost', :soturi => 'warrior', :sisu => 'stoic determination' }
 fav_movies = { 'Before Sunset' => 2004, 'The Handmaiden' => 2016, 'Rear Window' => 1954 }
 world_cities = { :london => 8136000, :tokyo => 9273000, :hong_kong => 7392000 }
-age_cousins = { :jack => 22, :sandra => 28, :helen => 26, :patrick => 22 }
+age_cousins = { "Jack" => 22, "Sandra" => 28, "Helen" => 26, "Patrick" => 22 }
 
 puts finnish_dictionary[:aave]
 puts fav_movies['Rear Window']
@@ -64,3 +75,25 @@ puts fav_movies
 
 world_cities[:london] = 9000000
 puts world_cities
+
+def get_sum_total_pop(hash)
+  world_cities_total_pop = 0
+  hash.each do |city, population|
+    world_cities_total_pop += population
+  end
+  return world_cities_total_pop
+end
+
+puts get_sum_total_pop(world_cities)
+
+def get_age_cousins_for_message(hash)
+  hash.each do |cousin, age|
+      if age >= 30
+        puts "#{cousin} is old."
+      else
+        puts "#{cousin} is young."
+    end
+  end
+end
+
+puts get_age_cousins_for_message(age_cousins)
